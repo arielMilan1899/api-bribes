da3ac2d90af1d0695039a90e8aa653a55eb139fb
abarrios <radke1899@gmail.com>
2021-09-06 16:34:49 -0400

    Adding fullDeckCount algorithm

--- src/bribes/algorithms.js
+++ src/bribes/algorithms.js
@@ -1,6 +1,21 @@
 //Bribes algorithms.js
 const fullDeckCount = (cards) => {
+    const cardTypes = ['hearts', 'clubs', 'diamonds', 'spades'];
+    const cardNumbers = Array.from({length: 9}, (_, index) => index + 2).concat(['J', 'Q', 'K', 'A']);
+    let count = Number.MAX_VALUE;
 
+    for (let cardType of cardTypes) {
+        for (let cardNumber of cardNumbers) {
+
+            const cardCount = cards.filter(card => card.suit === cardType && card.value === cardNumber).length;
+
+            if (cardCount < count) {
+                count = cardCount;
+            }
+        }
+    }
+
+    return count;
 };
 
 const minBribes = (queue) => {
