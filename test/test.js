c11998fde37f2984b4dbd475cc8dd64275f76a8c
abarrios <radke1899@gmail.com>
2021-09-06 17:05:27 -0400

    Adding tests for fullDeckCount algorithm

--- test/test.js
+++ test/test.js
@@ -1,10 +1,11 @@
 const chai = require("chai");
+const data = require("./datasets");
 const {minBribes, fullDeckCount} = require("../src/bribes/algorithms");
 
 // Assertion
 chai.should();
 
-describe("Test minBribes algorithm", () => {
+describe("Testing minBribes algorithm", () => {
     it('should return 0 if the queue is sorted', function () {
         const bribes = minBribes([1, 2, 3, 4]);
         bribes.should.equal(0);
@@ -16,5 +17,19 @@ describe("Test minBribes algorithm", () => {
     it('should return "Too chaotic" if element bribed more than two elements', function () {
         const bribes = minBribes([4, 1, 2, 3]);
         bribes.should.equal('Too chaotic');
+    });
+});
+describe("Testing fullDeckCount algorithm", () => {
+    it('should return 0', function () {
+        const count = fullDeckCount(data.dataset1);
+        count.should.equal(0);
+    });
+    it('should return 0', function () {
+        const count = fullDeckCount(data.dataset2);
+        count.should.equal(0);
+    });
+    it('should return 0', function () {
+        const count = fullDeckCount(data.dataset3);
+        count.should.equal(2);
     });
 });
